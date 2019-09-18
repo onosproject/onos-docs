9e201afb06cc26a01b04930236b3ba49e57be3c4
Adib Rastegarnia <adib.rastegarnia@gmail.com>
2019-09-18 14:40:58 -0400

    Fix build docs to point to the correct repo (#4)

--- build/build_docs.sh
+++ build/build_docs.sh
@@ -2,7 +2,7 @@
 shopt -s extglob
 git config user.name "adibrastegarnia"
 git config user.email "arastega@purdue.edu"
-git remote add gh-token "https://${GH_TOKEN}@github.com/multirepo-docs/root-docs.git";
+git remote add gh-token "https://${GH_TOKEN}@github.com/onosproject/onos-docs.git";
 git fetch gh-token && git fetch gh-token gh-pages:gh-pages;
 CURRENT_PATH=$PWD
 mkdocs gh-deploy -v --clean --remote-name gh-token;
