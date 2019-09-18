7f7a2b5d6b5c95beb53e4354ccaf11562a13da6a
adibrastegarnia <adib.rastegarnia@gmail.com>
2019-09-18 14:26:00 -0400

    Update build docs script and travis to trigger the mkdocs deployment script

--- build/build_docs.sh
+++ build/build_docs.sh
@@ -1,1 +1,8 @@
 #!/bin/bash
+shopt -s extglob
+git config user.name "adibrastegarnia"
+git config user.email "arastega@purdue.edu"
+git remote add gh-token "https://${GH_TOKEN}@github.com/multirepo-docs/root-docs.git";
+git fetch gh-token && git fetch gh-token gh-pages:gh-pages;
+CURRENT_PATH=$PWD
+mkdocs gh-deploy -v --clean --remote-name gh-token;
